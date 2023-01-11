2060874c7407875b4b85e4ce4749968f422b87a4
Sharif <sharifhosen73@gmail.com>
2023-01-11 21:33:58 +0600

    added by sharif

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Conduct from "../Conduct/Conduct";
+import Integrations from "../Integrations/Integrations";
+import Happens from "./../Happens/Happens";
+
+const Home = () => {
+  return (
+    <div>
+      <Conduct />
+      <Happens />
+      <Integrations />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Pages/Home/Integrations/Integrations.js
@@ -0,0 +1,26 @@
+import React from "react";
+import image from "../../../images/Integrations-Calendly__1_.webp";
+
+const Integrations = () => {
+  return (
+    <div className="flex flex-col lg:flex-row items-center lg:w-4/5 mx-auto lg:h-[620px] bg-gray-100 px-10">
+      <div className="text-left lg:w-2/5">
+        <h1 className="text-5xl font-bold text-cyan-900 lg:mb-14 my-8">
+          Integrations and <br /> Extensions
+        </h1>
+        <p className="text-xl text-gray-500 mb-8 lg:mb-14">
+          Boost productivity with integrations that fold right into your
+          workflow.
+        </p>
+        <button className="bg-sky-600 border-none px-5 py-3 text-xl font-bold text-white hover:bg-sky-700 rounded-lg mb-10">
+          Explore integrations
+        </button>
+      </div>
+      <div className="lg:w-3/5 ">
+        <img className="h-96 lg:h-[580px] " src={image} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Integrations;
