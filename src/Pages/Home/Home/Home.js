3d22c442f0db14df38eec0eb2e42db073e808152
muntasir <sajeebmuntasir0@gmail.com>
2023-01-11 23:49:45 +0600

    first commit and review added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Conduct from "../Conduct/Conduct";
 import Integrations from "../Integrations/Integrations";
+import Reviews from "../Reviews/Reviews";
 import Happens from "./../Happens/Happens";
 
 const Home = () => {
   return (
     <div>
       <Conduct />
+      <Reviews />
       <Happens />
       <Integrations />
     </div>
