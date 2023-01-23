c1a35e5daa1633ada377e228a1154cf970b513cf
Amran Hossain <amran.h.akash@gmail.com>
2023-01-23 15:25:53 +0600

    price card for mobile completed

--- src/Pages/Pricing/Pricing.js
+++ src/Pages/Pricing/Pricing.js
@@ -4,7 +4,7 @@ import { FaCheck, FaRegTimesCircle } from 'react-icons/fa';
 const Pricing = () => {
     return (
         <div>
-            <div className='my-32 '>
+            <div className='md:my-32 my-20'>
                 <h1 className='md:text-4xl text-2xl font-bold text-center'>Effective & Affordable</h1>
                 <p className='text-center mt-6 text-lg'>Free trial of one month no credit card needed.</p>
                 <p className='text-center text-lg'>You will be charged in a yearly basis, so you can cancel anytime.</p>
@@ -32,7 +32,6 @@ const Pricing = () => {
                 </div>
             </div>
 
-            {/* Table */}
 
             <div className='my-6 hidden md:block'>
                 <h1 className='font-bold text-2xl'>Features</h1>
@@ -110,12 +109,12 @@ const Pricing = () => {
 
             </div>
 
-            {/* For Mobile */}
+            {/*----- For Mobile -----*/}
 
             <div className='md:hidden'>
 
-
-                <div className="card border-4 mx-2 pl-6">
+                {/* Basic Plan Card */}
+                <div className="card border-4 mx-2 pl-6 mb-8 py-6">
                     <div >
                         <h2 className="card-title text-2xl font-bold">Basic</h2>
                         <p className='my-4 text-lg'> <span className='text-lg font-bold'>$9</span> per month</p>
@@ -155,11 +154,149 @@ const Pricing = () => {
                             Round-robin or directory listing</p> </div>
                         <div className='flex justify-end pr-8 w-1/3 font-semibold items-center'><p><FaCheck className='text-emerald-600' /></p></div>
                     </div>
-                    <hr />
-
+                    <div className='mr-6'>
+                        <button className='btn px-6 border-none bg-emerald-400 hover:bg-emerald-600 w-full '>Sign Up</button>
+                    </div>
 
                 </div>
 
+                {/* Professional Plan Card */}
+                <div className="card border-4 mx-2 pl-6 mb-8 py-6">
+                    <div >
+                        <h2 className="card-title text-2xl font-bold">Professional</h2>
+                        <p className='my-4 text-lg'> <span className='text-lg font-bold'>$19</span> per month</p>
+                    </div>
+
+                    <div className='flex flex-row mb-4'>
+                        <div className='w-2/3'> <h1 className='text-xl font-semibold'>Booking pages</h1><p>Number of landing pages to show availability</p> </div>
+                        <div className='flex justify-end pr-8 w-1/3 font-semibold items-center'><p>4</p></div>
+                    </div>
+                    <hr />
+                    <div className='flex flex-row mb-4'>
+                        <div className='w-2/3'> <h1 className='text-xl font-semibold'>Bookings per month</h1><p>
+                            Maximum number of bookings per month</p> </div>
+                        <div className='flex justify-end pr-8 w-1/3 font-semibold items-center'><p>5000</p></div>
+                    </div>
+                    <hr />
+                    <div className='flex flex-row mb-4'>
+                        <div className='w-2/3'> <h1 className='text-xl font-semibold'>Integrations</h1><p>
+                            Zoom, Google Meet, Slack, Zapier, etc.</p> </div>
+                        <div className='flex justify-end pr-8 w-1/3 font-semibold items-center'><p>6</p></div>
+                    </div>
+                    <hr />
+                    <div className='flex flex-row mb-4'>
+                        <div className='w-2/3'> <h1 className='text-xl font-semibold'>One-on-one meetings</h1><p>
+                            Good for sales execs, realtors, HR consultants.</p> </div>
+                        <div className='flex justify-end pr-8 w-1/3 font-semibold items-center'><p><FaCheck className='text-emerald-600' /></p></div>
+                    </div>
+                    <hr />
+                    <div className='flex flex-row mb-4'>
+                        <div className='w-2/3'> <h1 className='text-xl font-semibold'>Group meetings</h1><p>
+                            Good for yoga classes, virtual events</p> </div>
+                        <div className='flex justify-end pr-8 w-1/3 font-semibold items-center'><p><FaCheck className='text-emerald-600' /></p></div>
+                    </div>
+                    <hr />
+                    <div className='flex flex-row mb-4'>
+                        <div className='w-2/3'> <h1 className='text-xl font-semibold'>Team meetings</h1><p>
+                            Round-robin or directory listing</p> </div>
+                        <div className='flex justify-end pr-8 w-1/3 font-semibold items-center'><p><FaCheck className='text-emerald-600' /></p></div>
+                    </div>
+                    <div className='mr-6'>
+                        <button className='btn px-6 border-none bg-emerald-400 hover:bg-emerald-600 w-full '>Sign Up</button>
+                    </div>
+                </div>
+
+                {/* Business Plan Card */}
+                <div className="card border-4 mx-2 pl-6 mb-8 py-6">
+                    <div >
+                        <h2 className="card-title text-2xl font-bold">Business</h2>
+                        <p className='my-4 text-lg'> <span className='text-lg font-bold'>$49</span> per month</p>
+                    </div>
+
+                    <div className='flex flex-row mb-4'>
+                        <div className='w-2/3'> <h1 className='text-xl font-semibold'>Booking pages</h1><p>Number of landing pages to show availability</p> </div>
+                        <div className='flex justify-end pr-8 w-1/3 font-semibold items-center'><p>8</p></div>
+                    </div>
+                    <hr />
+                    <div className='flex flex-row mb-4'>
+                        <div className='w-2/3'> <h1 className='text-xl font-semibold'>Bookings per month</h1><p>
+                            Maximum number of bookings per month</p> </div>
+                        <div className='flex justify-end pr-8 w-1/3 font-semibold items-center'><p>10000</p></div>
+                    </div>
+                    <hr />
+                    <div className='flex flex-row mb-4'>
+                        <div className='w-2/3'> <h1 className='text-xl font-semibold'>Integrations</h1><p>
+                            Zoom, Google Meet, Slack, Zapier, etc.</p> </div>
+                        <div className='flex justify-end pr-8 w-1/3 font-semibold items-center'><p>12</p></div>
+                    </div>
+                    <hr />
+                    <div className='flex flex-row mb-4'>
+                        <div className='w-2/3'> <h1 className='text-xl font-semibold'>One-on-one meetings</h1><p>
+                            Good for sales execs, realtors, HR consultants.</p> </div>
+                        <div className='flex justify-end pr-8 w-1/3 font-semibold items-center'><p><FaCheck className='text-emerald-600' /></p></div>
+                    </div>
+                    <hr />
+                    <div className='flex flex-row mb-4'>
+                        <div className='w-2/3'> <h1 className='text-xl font-semibold'>Group meetings</h1><p>
+                            Good for yoga classes, virtual events</p> </div>
+                        <div className='flex justify-end pr-8 w-1/3 font-semibold items-center'><p><FaCheck className='text-emerald-600' /></p></div>
+                    </div>
+                    <hr />
+                    <div className='flex flex-row mb-4'>
+                        <div className='w-2/3'> <h1 className='text-xl font-semibold'>Team meetings</h1><p>
+                            Round-robin or directory listing</p> </div>
+                        <div className='flex justify-end pr-8 w-1/3 font-semibold items-center'><p><FaCheck className='text-emerald-600' /></p></div>
+                    </div>
+                    <div className='mr-6'>
+                        <button className='btn px-6 border-none bg-emerald-400 hover:bg-emerald-600 w-full '>Sign Up</button>
+                    </div>
+                </div>
+
+                {/* Enterprise Plan Card */}
+                <div className="card border-4 mx-2 pl-6 mb-8 py-6">
+                    <div >
+                        <h2 className="card-title text-2xl font-bold">Enterprise</h2>
+                        <p className='my-4 text-lg'> <span className='text-lg font-bold'> </span> Let's talk</p>
+                    </div>
+
+                    <div className='flex flex-row mb-4'>
+                        <div className='w-2/3'> <h1 className='text-xl font-semibold'>Booking pages</h1><p>Number of landing pages to show availability</p> </div>
+                        <div className='flex justify-end pr-8 w-1/3 font-semibold items-center'><p>Custom</p></div>
+                    </div>
+                    <hr />
+                    <div className='flex flex-row mb-4'>
+                        <div className='w-2/3'> <h1 className='text-xl font-semibold'>Bookings per month</h1><p>
+                            Maximum number of bookings per month</p> </div>
+                        <div className='flex justify-end pr-8 w-1/3 font-semibold items-center'><p>Custom</p></div>
+                    </div>
+                    <hr />
+                    <div className='flex flex-row mb-4'>
+                        <div className='w-2/3'> <h1 className='text-xl font-semibold'>Integrations</h1><p>
+                            Zoom, Google Meet, Slack, Zapier, etc.</p> </div>
+                        <div className='flex justify-end pr-8 w-1/3 font-semibold items-center'><p>Custom</p></div>
+                    </div>
+                    <hr />
+                    <div className='flex flex-row mb-4'>
+                        <div className='w-2/3'> <h1 className='text-xl font-semibold'>One-on-one meetings</h1><p>
+                            Good for sales execs, realtors, HR consultants.</p> </div>
+                        <div className='flex justify-end pr-8 w-1/3 font-semibold items-center'><p><FaCheck className='text-emerald-600' /></p></div>
+                    </div>
+                    <hr />
+                    <div className='flex flex-row mb-4'>
+                        <div className='w-2/3'> <h1 className='text-xl font-semibold'>Group meetings</h1><p>
+                            Good for yoga classes, virtual events</p> </div>
+                        <div className='flex justify-end pr-8 w-1/3 font-semibold items-center'><p><FaCheck className='text-emerald-600' /></p></div>
+                    </div>
+                    <hr />
+                    <div className='flex flex-row mb-4'>
+                        <div className='w-2/3'> <h1 className='text-xl font-semibold'>Team meetings</h1><p>
+                            Round-robin or directory listing</p> </div>
+                        <div className='flex justify-end pr-8 w-1/3 font-semibold items-center'><p><FaCheck className='text-emerald-600' /></p></div>
+                    </div>
+                    <div className='mr-6'>
+                        <button className='btn px-6 border-none bg-emerald-400 hover:bg-emerald-600 w-full '>Sign Up</button>
+                    </div>
+                </div>
 
             </div>
 
